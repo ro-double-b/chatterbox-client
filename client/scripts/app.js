e973eb88d1d3b7258c4d7d1a27e1441a1816f368
ro-double-b <robb.marsee@gmail.com>
2016-06-20 19:47:44 -0700

    complete sending app behavior test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,69 +1,111 @@
-$(document).ready(function() {
-// YOUR CODE HERE:
-  var obj = $.get('https://api.parse.com/1/classes/messages', function(data) {
-    console.log(data);
-  });
+    // var message = {
+    //   username: '',
+    //   text: '',
+    //   roomname: ''
+    // };
 
+var app = {
+  init: function() {
 
-  var getMessage = function() {
-    var message = {
-      username: '',
-      text: '',
-      roomname: ''
-    };
+  },
 
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/messages',
-      type: 'GET',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        for (var i = 0; i < data.results.length; i++) {
-          $('#chats').append((data.results[i].text + '<br>'));
-          console.log('chatterbox: Message Recieved');
-        }
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-  };
+  servers: 'https://api.parse.com/1/classes/messages',
 
-  getMessage();
-
-  $('#get').on('click', function() {
-    var chatSubmit = $('input').val();
-    if (escapeHtml(chatSubmit) === chatSubmit) {
-      postMessage(escapeHtml(chatSubmit));
-    } else {
-      console.log('error ');
-    }
-
-    console.log(escapeHtml(chatSubmit));
-  });
-
-  var postMessage = function(chatSubmit) {
-    // console.log(chatSubmit)
-    var message = {
-      username: 'hello',
-      text: chatSubmit,
-      roomname: 'random'
-    };
-
+  send: function(chatMessage) {
+  
     $.ajax({
       url: 'https://api.parse.com/1/classes/messages',
       type: 'POST',
-      data: JSON.stringify(message),
+      data: JSON.stringify(chatMessage),
       contentType: 'application/json',
       success: function (data) {
-        $('#chats').append(chatSubmit);      
-        console.log('chatterbox: Message Sent');
+        console.log('chatterbox: Message sent');
       },
       error: function (data) {
-        console.error('chatterbox: Failed to send message', data);
+        console.log('chatterbox: Failed to send message', data);
       }
     });
-  };
+  },
+
+  fetch: function() {
+
+  }
+
+
+};
+
+
+// $(document).ready(function() {
+//   $('#get').on('click', function() {
+//     var chatMessage = {}
+//     chatMessage.message = $('input').val()
+//     app.send(chatMessage);
+//   });
+// });
+
+// $(document).ready(function() {
+// // YOUR CODE HERE:
+
+
+//   var getMessage = function() {
+//     var message = {
+//       username: '',
+//       text: '',
+//       roomname: ''
+//     };
+
+//     $.ajax({
+//       url: 'https://api.parse.com/1/classes/messages',
+//       type: 'GET',
+//       data: JSON.stringify(message),
+//       contentType: 'application/json',
+//       success: function (data) {
+//         for (var i = 0; i < data.results.length; i++) {
+//           $('#chats').append((data.results[i].text + '<br>'));
+//           console.log('chatterbox: Message Recieved');
+//         }
+//       },
+//       error: function (data) {
+//         console.error('chatterbox: Failed to send message', data);
+//       }
+//     });
+//   };
+
+//   getMessage();
+
+//   $('#get').on('click', function() {
+//     var chatSubmit = $('input').val();
+//     if (escapeHtml(chatSubmit) === chatSubmit) {
+//       postMessage(escapeHtml(chatSubmit));
+//     } else {
+
+//     }
+
+//     console.log(escapeHtml(chatSubmit));
+//   });
+
+//   var postMessage = function(chatSubmit) {
+//     // console.log(chatSubmit)
+//     var message = {
+//       username: 'hello',
+//       text: chatSubmit,
+//       roomname: 'random'
+//     };
+
+//     $.ajax({
+//       url: 'https://api.parse.com/1/classes/messages',
+//       type: 'POST',
+//       data: JSON.stringify(message),
+//       contentType: 'application/json',
+//       success: function (data) {
+//         $('#chats').append(chatSubmit);      
+//       console.log('chatterbox: Message Sent');
+//       },
+//       error: function (data) {
+//         console.error('chatterbox: Failed to send message', data);
+//       }
+//     });
+//   };
 
 
  var entityMap = {
@@ -82,4 +124,4 @@ $(document).ready(function() {
   }
 
 
-});
+// });
