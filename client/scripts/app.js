fee43b849b47cfef53cd8db4171db3d078e0f813
ro-double-b <robb.marsee@gmail.com>
2016-06-21 15:27:24 -0700

    implement chat room drop down box population

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -15,6 +15,7 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         app.addMessage(chatMessage);
+        console.log(chatMessage)
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
@@ -29,6 +30,7 @@ var app = {
       type: 'GET',
       success: function (data) {
         appendChat(data.results);
+        app.addRoom(data.results);
         console.log('chatterbox: Message Recieved');
       },
       error: function (data) {
@@ -42,23 +44,35 @@ var app = {
   },
 
   addMessage: function(data) {
-    $('#chats').prepend('<div>' + escapeHtml(data.username) + ' ' + escapeHtml(data.text) + '</div>');
-    console.log($('#chats'));
+    if (escapeHtml(data.username) === data.username && escapeHtml(data.roomname) === data.roomname && escapeHtml(data.text) === data.text) {
+      $('#chats').prepend('<div><span class="' + escapeHtml(data.username) + ' ' + escapeHtml(data.roomname) + '">' + escapeHtml(data.username) + ' ' + escapeHtml(data.text) + '</div>');
+    }
   },
 
-  addRoom: function(room) {
-
-    $('#roomSelect').append('<option value="' + room + '">' + room + '</option>');
-
+  addRoom: function(chatMessage) {
+    var roomArr = [];
+    for (var i = 0; i < chatMessage.length; i++) {
+      if (roomArr.indexOf(chatMessage[i].roomname) === -1 && chatMessage[i].roomname !== "") { 
+        roomArr.push(chatMessage[i].roomname);
+      }
+    }
+    for (var i = 0; i < roomArr.length; i++) {
+      $('#roomSelect').append('<option>' + roomArr[i] + '</option>');
+    }
   },
 
   addFriend: function() {
   
     $(document).on('click', 'span', function() {
       var username = $(this).attr('class');
-      $('span').each(function() {
+      $('.user').each(function() {
         if ($(this).attr('class') === username) {
-          $(this).toggleClass('bold');
+          $(this).toggleClass('addFriend');
+          if ($(this).hasClass('addFriend')) {
+            $('.addFriend').closest('div').find('.text').addClass('bold');
+          } else {
+            $('.bold').removeClass('bold');
+          } 
         }
       });
     });
@@ -69,10 +83,11 @@ app.fetch();
 
 var appendChat = function(data) {
   for (var i = 0; i < data.length; i++) {
-    $('#chats').append('<div><span class="' + escapeHtml(data[i].username) + '">' + escapeHtml(data[i].username) + '</span> ' 
-      + escapeHtml(data[i].text) + '</div>');
-
-    '<span class="' + escapeHtml(data[i].roomname) + '">' + escapeHtml(data[i].roomname) + '</span> '
+    if (escapeHtml(data[i].username) === data[i].username && escapeHtml(data[i].roomname) === data[i].roomname && escapeHtml(data[i].text) === data[i].text) {
+      $('#chats').append('<div><span class="user ' + escapeHtml(data[i].username) + ' ' + escapeHtml(data[i].roomname) + '">' + escapeHtml(data[i].username) + '</span> ' +
+      '<span class="text">' + escapeHtml(data[i].text) + '</span>' + '</div>');
+    }
+    // '<span class="' + escapeHtml(data[i].roomname) + '">' + escapeHtml(data[i].roomname) + '</span> '
   }
 };
 
@@ -92,83 +107,3 @@ var escapeHtml = function(string) {
     return entityMap[s];
   });
 };
-          
-            // $(document).on('click', 'button', function() {
-            //   var obj = {};
-            //   obj.username = $('#user').val();
-            //   obj.text = $('#input').val();
-            //   obj.roomname = $('#chatRoom').val();
-            //   app.send(obj);
-            // });
-          
-    // $('').on('click', function() {
-    //   alert($(this).attr('class'))
-    // })
-
-
-// $(document).ready(function() {
-//   // console.log($('#chats').children().length);
-//   $('div').on('click', function() {
-//     console.log($(this));
-//   });
-// });
-
-
-
-// $('#chats').append(messages);
-// generateChats(array);
-
-// app.fetch();
-// $(document).ready(function() {
-//   $('#get').on('click', function() {
-//     var chatMessage = {}
-//     chatMessage.message = $('input').val()
-//     app.send(chatMessage);
-//   });
-// });
-
-// $(document).ready(function() {
-// // YOUR CODE HERE:
-
-
-
-//   getMessage();
-
-//   $('#get').on('click', function() {
-//     var chatSubmit = $('input').val();
-//     if (escapeHtml(chatSubmit) === chatSubmit) {
-//       postMessage(escapeHtml(chatSubmit));
-//     } else {
-
-//     }
-
-//     console.log(escapeHtml(chatSubmit));
-//   });
-
-//   var postMessage = function(chatSubmit) {
-//     // console.log(chatSubmit)
-//     var message = {
-//       username: 'hello',
-//       text: chatSubmit,
-//       roomname: 'random'
-//     };
-
-//     $.ajax({
-//       url: 'https://api.parse.com/1/classes/messages',
-//       type: 'POST',
-//       data: JSON.stringify(message),
-//       contentType: 'application/json',
-//       success: function (data) {
-//         $('#chats').append(chatSubmit);      
-//       console.log('chatterbox: Message Sent');
-//       },
-//       error: function (data) {
-//         console.error('chatterbox: Failed to send message', data);
-//       }
-//     });
-//   };
-
-
-
-
-// });
